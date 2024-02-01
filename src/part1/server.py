c9478acae3fdd7271924986a827eda22e58a9052
Connor M <cmeyer8@hotmail.ca>
2024-02-01 18:34:34 -0500

    finished retire reuse and getNewIp

--- src/part1/server.py
+++ src/part1/server.py
@@ -1,15 +1,37 @@
+currentlyUsedIp = []
+retiredIp = []
+
+nextIp = [0, 0, 0, 0]
+
 # generates new ip
 def generatesNewIP() -> str:
     pass
 
+
 # removes given ip from active list
-def retireIp(activeList, retiredList):
-    pass
-# checks recently used list when we need a new ip
-def reuseIp():
-    pass
+def retireIp(ip: str, activeList: List[str], retiredList: List[str]) -> int:
+    try:
+        activeList.remove(ip)
+        retiredList.append(ip)
+        return 1
+    except(ValueError): 
+        return -1
+
+# checks recently used list when we need a new ip # will return "-1" if list is empty
+def reuseIp(unusedList) -> str:
+    if len(unusedList) > 0:
+        return unusedList.pop(0)
+    else:
+        return "-1"
+
 
 
 # figures out if a new ip needs to be generated or if one can be reused
 def getNewIp():
-    pass
+    tempIp = reuseIp(retiredIp)
+    if(tempIp != "-1"):
+        return tempIp
+    else:
+        return generatesNewIP()
+
+#test code
