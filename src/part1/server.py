64d34ac7e852dff1e5fbcc4796c9a5f412d9aea0
Connor M <cmeyer8@hotmail.ca>
2024-02-01 17:42:14 -0500

    function outlines

---
+++ src/part1/server.py
@@ -0,0 +1,15 @@
+# generates new ip
+def generatesNewIP() -> str:
+    pass
+
+# removes given ip from active list
+def retireIp(activeList, retiredList):
+    pass
+# checks recently used list when we need a new ip
+def reuseIp():
+    pass
+
+
+# figures out if a new ip needs to be generated or if one can be reused
+def getNewIp():
+    pass
