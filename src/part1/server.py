7c8c2813c9d2447309b9970aa8964cee434d25a8
Ethan Leir <eleir9267@gmail.com>
2024-02-01 18:38:16 -0500

    User input changes.

--- src/part1/server.py
+++ src/part1/server.py
@@ -3,13 +3,59 @@ def generatesNewIP() -> str:
     pass
 
 # removes given ip from active list
-def retireIp(activeList, retiredList):
+def retireIp(ipAddr):
     pass
 # checks recently used list when we need a new ip
-def reuseIp():
+def reuseIp(ipAddr):
     pass
 
 
 # figures out if a new ip needs to be generated or if one can be reused
 def getNewIp():
     pass
+
+def getIpStatus(ipAddr):
+    pass
+
+if __name__ == '__main__':
+    # Command : requiresIp, callout
+    commandOpts = {
+            "ASK" : (False, getNewIp),
+            "RENEW" : (True, reuseIp),
+            "RELEASE" : (True, retireIp),
+            "STATUS" : (True, getIpStatus)
+            }
+    while True:
+        commandIn = input("Please input a command:").strip().split()
+
+        if len(commandIn) < 1:
+            print("No command entered.")
+            continue
+
+        commandName = commandIn[0].upper()
+
+        if not commandName in commandOpts:
+            print("Illegal command entered.")
+            continue
+
+        commandOpt = commandOpts[commandIn[0].upper()]
+
+        requiresIp, callout = commandOpt
+        if (requiresIp and (len(commandIn) != 2)) \
+                or (not requiresIp and (len(commandIn) != 1)):
+            print("Illegal command arguments.")
+            continue
+
+        if requiresIp:
+            # Check that Ip is valid
+            if len([x for x in commandIn[1].split('.') if (len(x) <= 3) \
+                    and x.isnumeric() and (0 <= int(x) <= 255)]) != 4:
+                print("Invalid Ip address, should be from 0.0.0.0 to 255.255.255.255")
+                continue
+
+            # Error checking lets leading/trailing '.' through.
+            if callout(commandIn[1].strip('.')) != -1:
+                print("Command failed.")
+        else:
+            if callout() != -1:
+                print("Command failed.")
